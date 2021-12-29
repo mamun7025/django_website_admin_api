ded7f6e9b2313f1456bd8f7b1ef4f56644ecb287
muzhirul <muzhirulislam@gmail.com>
2021-12-29 17:05:31 +0600

    product details class, url

--- product_app/api/views.py
+++ product_app/api/views.py
@@ -2,11 +2,35 @@ from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from rest_framework import status
 from rest_framework import generics
+from django.http import JsonResponse
 from product_app.models import Product, ProductKeySpecs, ProductDetailSpecs, ProductDetailSpecsDtl
 from product_app.api.serializers import ProductSerializer, ProductKeySpecsSerializer, ProductDetailSpecsSerializer, ProductDetailSpecsDtlSerializer
 
 
 class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # data = list(queryset.values())
+    # # data = queryset.values()
+    # JsonResponse(data, safe=False)
+
+    def getAll(request):
+        queryset = Product.objects.all()
+        serializer_class = ProductSerializer
+        # data = list(queryset.values())
+        # return JsonResponse(serializer_class.data)
+
+    def productDetails(request, pk):
+        product = Product.objects.get(pk=pk)
+        # serializer_class = ProductSerializer
+        data = {
+            'movies': product.productCode,
+            'name': product.productName,
+        }
+        return JsonResponse(data)
+
+
+class ProductDetails(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
